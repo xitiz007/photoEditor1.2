dce933f7546f58a333eb62d3ed3ed2ad5345b790
kshitiz baniya <47114384+xitiz007@users.noreply.github.com>
2019-07-01 12:32:11 +0545

    Delete sratch.py

--- sratch.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector
-connect = mysql.connector.connect(host="localhost",user="root",passwd="aezakmi",database="students")
-cursor = connect.cursor()
-cursor.execute("select * from studentsinfo")
-for i in cursor:
-    print(i)
