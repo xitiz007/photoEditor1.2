4b7d7906c8cf4f19e44f4c998f0d94553b004d48
kshitiz baniya <47114384+xitiz007@users.noreply.github.com>
2019-09-10 17:47:48 +0545

    video capture

--- scratch.py
+++ scratch.py
@@ -3,6 +3,7 @@ from PyQt5.QtGui import QIcon,QIntValidator
 from PyQt5 import QtCore
 from tkinter import *
 import cv2
+import os
 
 class Options(QWidget):
     def __init__(self):
@@ -53,7 +54,26 @@ class Options(QWidget):
         root.mainloop()
 
     def capVideo(self):
-        print('capvideo')
+        filter = "AVI (*.avi)"
+        name = QFileDialog.getSaveFileName(self,'Save Video','',filter)
+        path = name[0]
+        if(path):
+            video = cv2.VideoCapture(0)
+            fourcc = cv2.VideoWriter_fourcc(*'XVID')
+            videoWriter = cv2.VideoWriter(path,fourcc,23,(640,480))
+            if video.isOpened():
+                while (True):
+                    tof , frame = video.read()
+                    cv2.imshow('Video',frame)
+                    videoWriter.write(frame)
+                    key = cv2.waitKey(1)
+                    if key==27 :
+                        break
+                videoWriter.release()
+                video.release()
+                cv2.destroyAllWindows()
+
+
 
     def photoScript(self):
         self.obj2 = photoScript()
@@ -75,41 +95,88 @@ class photoScript(QWidget):
         button.setToolTip("Select Directory")
         button.setIcon(QIcon('browse.png'))
         button.setIconSize(QtCore.QSize(40,40))
+        button.clicked.connect(self.browseDirectory)
         vbox.addWidget(button)
-        path = QTextEdit()
-        path.setDisabled(True)
-        path.setToolTip("Path")
-        path.setMaximumHeight(40)
-        vbox.addWidget(path)
+        self.path = QTextEdit()
+        self.path.setDisabled(True)
+        self.path.setToolTip("Path")
+        self.path.setMaximumHeight(40)
+        vbox.addWidget(self.path)
         hbox = QHBoxLayout()
-        width = QLineEdit()
-        width.setValidator(QIntValidator())
-        width.setToolTip("Width")
-        width.setMinimumHeight(30)
-        hbox.addWidget(width)
-        height = QLineEdit()
-        height.setToolTip("Height")
-        height.setMinimumHeight(30)
-        height.setValidator(QIntValidator())
-        hbox.addWidget(height)
+        self.width = QLineEdit()
+        self.width.setValidator(QIntValidator())
+        self.width.setToolTip("Width")
+        self.width.setMinimumHeight(30)
+        hbox.addWidget(self.width)
+        self.height = QLineEdit()
+        self.height.setToolTip("Height")
+        self.height.setMinimumHeight(30)
+        self.height.setValidator(QIntValidator())
+        hbox.addWidget(self.height)
         vbox.addLayout(hbox)
         hbox2 = QHBoxLayout()
-        checkbox1 = QCheckBox()
-        checkbox1.setText(".JPG")
-        checkbox2 = QCheckBox()
-        checkbox2.setText(".PNG")
-        group = QButtonGroup()
-        group.addButton(checkbox1)
-        group.addButton(checkbox2)
-        hbox2.addItem(checkbox1)
-        hbox2.addItem(checkbox2)
+        label2 = QLabel()
+        label2.setText("Image Extension:")
+        self.radioButton1 = QRadioButton()
+        self.radioButton1.setChecked(True)
+        self.radioButton1.setText(".JPG")
+        self.radioButton2 = QRadioButton()
+        self.radioButton2.setText(".PNG")
+        self.checkBox = QCheckBox()
+        self.checkBox.setText("B&W")
+        self.checkBox.setToolTip("Black & White")
+        saveButton = QPushButton()
+        saveButton.setToolTip("Select where to save")
+        saveButton.setIconSize(QtCore.QSize(40,40))
+        saveButton.setIcon(QIcon('save.png'))
+        saveButton.clicked.connect(self.saveButtonClicked)
+        hbox2.addWidget(label2)
+        hbox2.addWidget(self.radioButton1)
+        hbox2.addWidget(self.radioButton2)
+        hbox2.addWidget(self.checkBox)
         vbox.addLayout(hbox2)
+        vbox.addWidget(saveButton)
+        convertButton = QPushButton()
+        convertButton.setText("Convert")
+        convertButton.setMinimumHeight(30)
+        convertButton.clicked.connect(self.convertButtonClicked)
+        vbox.addWidget(convertButton)
 
         self.setWindowIcon(QIcon('script.png'))
         self.setWindowTitle("Photo Scripting")
         self.setLayout(vbox)
         self.show()
 
+    def convertButtonClicked(self):
+        try:
+            if(self.file and self.saveFile):
+                path = self.file
+                path = path.replace('\\','/')
+                for files in os.listdir(self.file):
+                    if(files.endswith(".jpg") or files.endswith(".png") or files.endswith(".jpeg")):
+                        name=""
+                        image = cv2.imread(path+'/'+files)
+                        if(self.checkBox.isChecked()):
+                            image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+                        image = cv2.resize(image,(int(self.width.text()),int(self.height.text())))
+                        if(self.radioButton1.isChecked()):
+                            name = files[:files.index('.')] + ".jpg"
+                        elif(self.radioButton2.isChecked()):
+                             name = files[:files.index('.')] + ".png"
+                        cv2.imwrite(self.saveFile+'/'+name,image)
+
+
+
+        except:
+            print("Error")
+
+    def browseDirectory(self):
+        self.file = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        self.file = self.file.replace('/','\\')
+        self.path.setText(self.file)
+
+    def saveButtonClicked(self):
+        self.saveFile = str(QFileDialog.getExistingDirectory(self, "Select  Directory"))
 
 def call():
     global root
